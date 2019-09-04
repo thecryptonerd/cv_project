66b4f098e62836eadb3ea5b0e6327dee1dc84267
crypto nerd <SM@SMs-MacBook-Pro.local>
2019-09-04 14:30:15 +0100

    Adjusted the contact form

--- form.js
+++ form.js
@@ -1,23 +1,25 @@
 
 const modalControls = (() => {
-   //function to open modal
+   //this function opens modal btn
    const openModal = () => {
     modal.classList.remove('hide-modal');
 };
-//function to close modal
+//this function closes modal btn
 const closeModal = () => {
     modal.classList.add('hide-modal');
     email.value = '';
     name.value = '';
+    title.value = '';
     message.value = '';
     };
 
-//grab elements and asssign functions on action trigger
+//assigns function when action is triggered
 const openModalBtn = document.querySelector('.contact-modal-open-btn').addEventListener('click', openModal);
     const closeModalBtn = document.querySelector('#close-btn');
     closeModalBtn.addEventListener('click', closeModal);
     const modal = document.querySelector('#modal-with-form'); 
     const name = document.querySelector('#name'); 
     const email = document.querySelector('#email'); 
+    const title = document.querySelector('#title');
     const message = document.querySelector('#message'); 
 })();
